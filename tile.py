9c532c575b815e355e6fc6f5c074db4285c18e04
GavinMonteiro <gtmontei@uvm.edu>
2026-02-23 00:50:15 -0500

    Testing out making tiles.

---
+++ tile.py
@@ -0,0 +1,26 @@
+import arcade
+
+TILE_WIDTH = 80
+TILE_HEIGHT = 100
+
+
+class Tile(arcade.Sprite):
+    def __init__(self, x, y, color, value):
+        super().__init__(hit_box_algorithm="None")
+
+        self.value_color = tuple(color)
+        self.value = value
+
+        self.texture = arcade.make_soft_square_texture(
+            80,
+            arcade.color.ANTI_FLASH_WHITE,
+            outer_alpha = 255
+        )
+
+        self.width = TILE_WIDTH
+        self.height = TILE_HEIGHT
+
+        self.center_x = x
+        self.center_y = y
+
+
