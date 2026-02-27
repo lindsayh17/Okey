c945ba718a6ac5e02acd8c54ddc63f2364ff9211
lindsayh17 <lhall11@uvm.edu>
2026-02-27 17:46:20 -0500

    Rounded button

--- views/title_view.py
+++ views/title_view.py
@@ -1,4 +1,5 @@
 import arcade
+import ui_components.button as button
 
 class TitleView(arcade.View):
     def __init__(self):
@@ -8,8 +9,25 @@ class TitleView(arcade.View):
         """ This is run once when we switch to this view """
         self.window.background_color = (1, 22, 56)
 
-        self.title_text = arcade.Text("Okey", self.window.width / 2, self.window.height / 2,
-                                      (255, 107, 107), font_size=50, anchor_x="center")
+        arcade.load_font("../assets/fonts/IrishGrover-Regular.ttf")
+
+        self.title_text = arcade.Text(
+            "Okey",
+            self.window.width / 2,
+            self.window.height / 2,
+            (255, 107, 107),
+            font_size= self.window.height * 0.25,
+            anchor_x="center",
+            font_name="Irish Grover"
+        )
+
+        self.play_button = button.Button(self.window.width / 3,
+                             self.window.height / 3,
+                             self.window.width / 6,
+                             self.window.height / 11,
+                             "Play",
+                             (255, 107, 107),
+                                         (1, 22, 56))
 
         # Reset the viewport, necessary if we have a scrolling game and we need
         # to reset the viewport back to the start so we can see what we draw.
@@ -21,6 +39,7 @@ class TitleView(arcade.View):
         self.title_text.draw()
         arcade.draw_text("Click to advance", self.window.width / 2, self.window.height / 2-75,
                          arcade.color.WHITE, font_size=20, anchor_x="center")
+        self.play_button.draw()
 
 def main():
     """ Main function """
