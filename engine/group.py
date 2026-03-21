0478d329fe4b65da53181e0d89fd1a930a80d1f2
ycolinta <ycolinta@uvm.edu>
2026-03-21 14:25:54 -0400

    Created a 'run' validity check for tile grouping

--- engine/group.py
+++ engine/group.py
@@ -11,3 +11,46 @@ class Group:
         self.points = 0
 
         self.evaluate()
+
+    def evaluate(self):
+        if self.is_run():
+            self.type = self.TYPE1
+            self.points = self.calculate_points()
+        elif self.is_set():
+            self.type = self.TYPE2
+            self.points = self.calculate_points()
+        else:
+            self.type = self.TYPE3
+            self.points = 0
+
+    def is_run(self):
+        if len(self.tiles) < 3:
+            return False
+        # extracting colors
+        colors = []
+        for tile in self.tiles:
+            colors.append(tile.color)
+        # ordering numbers in tile set
+        numbers = []
+        for tile in self.tiles:
+            numbers.append(tile.number)
+        numbers.sort()
+        # check if all tiles are one color
+        one_color = len(set(colors)) == 1
+
+        # check for consecutive order
+        consecutive_order = True
+        for i in range(len(numbers) - 1):
+            if numbers[i] + 1 != numbers[i + 1]:
+                consecutive_order = False
+                break
+
+        return one_color and consecutive_order # returns either True or False
+
+
+
+
+
+
+
+
