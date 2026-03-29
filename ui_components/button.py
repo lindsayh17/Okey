40f8f17c51f11e6ae2475c8fa4785873fea16508
lindsayh17 <lhall11@uvm.edu>
2026-03-29 13:55:39 -0400

    Pylint button

--- ui_components/button.py
+++ ui_components/button.py
@@ -5,36 +5,34 @@ class Button():
     """
     Creates a button using a rounded rectangle and text.
     """
-    def __init__(self, x, y, width, height, value, color, text_color):
-        self.center_x = x
-        self.center_y = y
-        self.width = width
-        self.height = height
+    def __init__(self, coords, dimensions, value, colors):
+        self.coordinates = {"x": coords[0], "y": coords[1]}
+        self.dimensions = {"width": dimensions[0], "height": dimensions[1]}
         self.text = value
-        self.radius = height // 4
+        self.radius = dimensions["height"] // 4
 
-        self.color = color
-        self.text_color = text_color
-        self.hover_color = arcade.color.SPRING_GREEN
-        self.current_color = self.color
+        self.colors = {"color": colors[0],
+                       "text_color": colors[1]}
 
         self.rect = None
 
     def draw(self):
         # draw a rounded rectangle
-        self.rect = rounded_rectangle.RoundedRectangle([self.center_x, self.center_y],
-                                                  [self.width, self.height],
+        self.rect = rounded_rectangle.RoundedRectangle([self.coordinates["x"],
+                                                        self.coordinates["y"]],
+                                                  [self.dimensions["width"],
+                                                   self.dimensions["height"]],
                                                   self.radius,
-                                                  self.color)
+                                                  self.colors["color"])
 
         # draw button text
         arcade.load_font("assets/fonts/Itim-Regular.ttf")
         txt = arcade.Text(
             self.text,
-            self.center_x,
-            self.center_y - self.height // 4,
-            self.text_color,
-            font_size= self.height * 0.6,
+            self.coordinates["x"],
+            self.coordinates["y"] - self.dimensions["height"] // 4,
+            self.colors["text_color"],
+            font_size= self.dimensions["height"] * 0.6,
             anchor_x="center",
             font_name="Itim"
         )
@@ -52,4 +50,4 @@ class Button():
         return self.rect.collided_with_rect(x, y)
 
     def set_color(self, color):
-        self.color = color
+        self.colors["color"] = color
