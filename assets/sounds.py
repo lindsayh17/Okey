823302f49b30460987c5d77506b127aefe5b4cfe
GavinMonteiro <gtmontei@uvm.edu>
2026-04-09 19:40:59 -0400

    Button sounds

--- assets/sounds.py
+++ assets/sounds.py
@@ -4,3 +4,5 @@ VOLUME = 0.5
 
 error = arcade.load_sound(":resources:/sounds/error3.wav")
 rock = arcade.load_sound(":resources:/sounds/rockHit2.wav")
+button = arcade.load_sound(":resources:/sounds/hurt2.wav")
+
