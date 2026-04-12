4af864f9c3b3a8910c6e4ae004a1e2bd782c37bc
lindsayh17 <lhall11@uvm.edu>
2026-04-12 11:58:33 -0400

    Fix tiles moving weirdly when open

--- assets/utils.py
+++ assets/utils.py
@@ -7,9 +7,9 @@ class Views(Enum):
     END = 'e'
     GAME = 'g'
 
-ROUNDS = 6 # change to 1 to debug
+ROUNDS = 1 # change to 1 to debug
 
 # make initial open 80 because player hand must be strictly greater than open score
 INITIAL_OPEN = 8
 
-STARS_OPEN = 100
+STARS_OPEN = 10
